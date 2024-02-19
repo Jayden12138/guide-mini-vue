78b368c3776f9da14efa20b911459fc1bb7a0e59
cactus <3184302575@qq.com>
2024-02-19 11:42:26 +0800

    feat: 24 shapeFlags

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,4 +1,4 @@
-import { isObject } from "../reactivity/src/shared/index"
+import { ShapeFlags } from "../shared/src/index";
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -8,9 +8,10 @@ export function render(vnode, container) {
 
 
 function patch(vnode, container) {
-    if (typeof vnode.type === "string") {
+    const { shapeFlag } = vnode;
+    if (shapeFlag & ShapeFlags.ELEMENT) {
         processElement(vnode, container)
-    }else if (isObject(vnode.type)) {
+    }else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
         processComponent(vnode, container)
     }
 }
@@ -31,11 +32,11 @@ function mountElement(vnode, container) {
    
     vnode.el = el;
 
-    const { children, props } = vnode
+    const { children, props, shapeFlag } = vnode
     // children: string | array
-    if(typeof children == 'string') {
+    if(shapeFlag & ShapeFlags.TEXT_CHILDREN) {
         el.textContent = children
-    } else if (Array.isArray(children)) {
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         mountChildren(vnode, el)
     }
 
