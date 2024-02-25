11bbb751049dd582ae35cc20e05892cfb5ca0acc
cactus1949 <djjff1102@gmail.com>
2024-02-25 10:13:00 +0800

    fix: 46 三种联合 更新 parse.sepc.ts simple element

--- packages/compiler-core/__test__/parse.spec.ts
+++ packages/compiler-core/__test__/parse.spec.ts
@@ -21,7 +21,8 @@ describe('parse', () => {
             const ast = baseParse("<div></div>");
             expect(ast.children[0]).toStrictEqual({
                 type: NodeTypes.ELEMENT,
-                tag: 'div'
+                tag: 'div',
+                children: []
             })
         })
     })
