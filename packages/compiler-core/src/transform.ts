68b9c8ceb3836f3516241ec1bd3ae112f4d306ac
cactus1949 <djjff1102@gmail.com>
2024-02-25 20:10:28 +0800

    feat: 48 transform-happy path add plugin

--- packages/compiler-core/src/transform.ts
+++ packages/compiler-core/src/transform.ts
@@ -1,27 +1,42 @@
-import { NodeTypes } from "./ast";
+export function transform(root, options) {
+    // 
+    const context = createTransformContext(root, options)
 
-export function transform(root){
+
     // 1. 遍历 - 深度优先搜索
     // 2. 修改 text - content
 
     // 1. 
-    traverseNode(root)
+    traverseNode(root, context)
+}
+
+function createTransformContext(root: any, options: any){
+    const context = {
+        root,
+        nodeTransforms: options.nodeTransforms || []
+    }
+    
+    return context
 }
 
 
-function traverseNode(node: any){
+function traverseNode(node: any, context){
+
+    const nodeTransforms = context.nodeTransforms;
+    for(let i = 0; i < nodeTransforms.length; i++){
+        const transform = nodeTransforms[i]
+        transform(node)
+    }
+
     console.log(node)
 
-    if(node.type == NodeTypes.TEXT){
-        node.content = node.content + " djj"
-    }
 
     const children = node.children;
 
     if(children){
         for(let i = 0; i < children.length; i++){
             const node = children[i]
-            traverseNode(node)
+            traverseNode(node, context)
         }
     }
 }
