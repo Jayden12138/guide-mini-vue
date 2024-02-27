355da4707747dd5948e03351505f452f9b58791b
cactus <3184302575@qq.com>
2024-02-27 15:03:04 +0800

    feat: 54 watchEffect-effect

--- packages2/runtime-core/src/scheduler.ts
+++ packages2/runtime-core/src/scheduler.ts
@@ -1,8 +1,9 @@
 const queue: any[] = []
+const activePreFlushCbs: any = []
 let isFlushPending = false
 const p = Promise.resolve();
 
-export function nextTick(fn) {
+export function nextTick(fn?) {
     return fn ? p.then(fn) : p
 }
 
@@ -11,6 +12,12 @@ export function queueJobs(job) {
     queue.push(job);
   }
   queueFlush();
+}
+
+export function queuePreFlushCb(fn) {
+  activePreFlushCbs.push(fn)
+  
+  queueFlush()
 }
 
 
@@ -22,8 +29,19 @@ function queueFlush() {
 
 function flushJobs() {
   isFlushPending = false
+
+  // 
+  flushPreFlushCbs();
+
+  // 组件渲染
   let job
   while ((job = queue.shift())) {
     job()
+  }
+}
+
+function flushPreFlushCbs() {
+  for (let index = 0; index < activePreFlushCbs.length; index++) {
+    activePreFlushCbs[index]();
   }
 }
