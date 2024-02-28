94ee64df1035555e9ac48e38474d7fdd084bf682
Jayden12138 <18993882884@163.com>
2024-02-28 23:54:30 +0800

    readonly

--- src/reactivity/src/reactive.ts
+++ src/reactivity/src/reactive.ts
@@ -1,22 +1,13 @@
-import { track, trigger } from './effect'
+import { mutableHandlers, readonlyHandlers } from './baseHandlers'
 
 export function reactive(raw) {
-	return new Proxy(raw, {
-		get(target, key) {
-			const res = Reflect.get(target, key)
+	return createActiveObject(raw, mutableHandlers)
+}
 
-			// 收集依赖
-			track(target, key)
+export function readonly(raw) {
+	return createActiveObject(raw, readonlyHandlers)
+}
 
-			return res
-		},
-		set(target, key, value) {
-			const res = Reflect.set(target, key, value)
-
-			// 触发依赖
-			trigger(target, key)
-
-			return res
-		},
-	})
+function createActiveObject(raw, baseHandlers) {
+	return new Proxy(raw, baseHandlers)
 }
