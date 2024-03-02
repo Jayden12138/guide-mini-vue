5e7a6b4390dc120b541ecfa747d9d0f5394cd566
Jayden12138 <18993882884@163.com>
2024-03-02 18:22:19 +0800

    init element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,24 @@
-import { h } from '../../lib/gudie-mini-vue.esm.js'
+import { h } from '../../lib/guide-mini-vue.esm.js'
 export const App = {
 	render() {
-		return h('div', 'hi, ' + this.msg)
+		return h(
+			'div',
+			{
+				id: 'root',
+				class: ['red', 'hard'],
+			},
+			// this
+			// 'hi, ' + this.msg
+
+			// string
+			// 'hi jayden',
+
+			// children
+			[
+				h('p', { class: 'red' }, 'hi'),
+				h('p', { class: 'blue' }, 'jayden'),
+			]
+		)
 	},
 	setup() {
 		return {
