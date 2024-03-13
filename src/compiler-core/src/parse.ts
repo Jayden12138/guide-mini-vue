86d0f33ed0ad3aa9ae78a8c245f0d40b49f481b1
Jayden12138 <18993882884@163.com>
2024-03-13 09:26:42 +0800

    parse element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast'
 
+const enum TagType {
+	Start,
+	End,
+}
+
 export function baseParse(content: string) {
 	const context = createContext(content)
 	return createRoot(parseChildren(context))
@@ -46,6 +51,10 @@ function parseChildren(context) {
 	let s = context.source
 	if (s.startsWith('{{')) {
 		node = parseInterpolation(context)
+	} else if (s.startsWith('<')) {
+		if (/[a-z]/.test(s[1])) {
+			node = parseElement(context)
+		}
 	}
 	nodes.push(node)
 
@@ -61,5 +70,28 @@ function createContext(content: string) {
 function createRoot(children) {
 	return {
 		children,
+	}
+}
+function parseElement(context: any) {
+	const element = parseTag(context, TagType.Start)
+	parseTag(context, TagType.End)
+
+	return element
+}
+function parseTag(context: any, type: TagType) {
+	const match: any = /^<\/?([a-z]*)/.exec(context.source)
+	const tag = match[1]
+
+	// <div
+	advanceBy(context, match[0].length)
+
+	// >
+	advanceBy(context, 1)
+
+	if (type === TagType.End) return
+
+	return {
+		type: NodeTypes.ELEMENT,
+		tag,
 	}
 }
