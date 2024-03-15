368cee9728cbf967145153eecbd19f00f8239f1f
Jayden12138 <18993882884@163.com>
2024-03-15 17:02:56 +0800

    effect - runner

--- src/reactivity/__test__/effect.spec.ts
+++ src/reactivity/__test__/effect.spec.ts
@@ -18,4 +18,17 @@ describe('reactive', () => {
 		obj.prop = 2
 		expect(dummy).toBe(2)
 	})
+
+	it('should return runner when call effect', () => {
+		let foo = 10
+		const runner = effect(() => {
+			foo++
+			return 'foo'
+		})
+
+		expect(foo).toBe(11)
+		const r = runner()
+		expect(foo).toBe(12)
+		expect(r).toBe('foo')
+	})
 })
