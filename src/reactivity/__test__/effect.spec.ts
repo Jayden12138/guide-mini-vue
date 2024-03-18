9521fdedebdd33f14320462e3bd265462fe33b89
Jayden12138 <18993882884@163.com>
2024-03-18 10:10:38 +0800

    effect - stop

--- src/reactivity/__test__/effect.spec.ts
+++ src/reactivity/__test__/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect, reactive } from '../src'
+import { effect, reactive, stop } from '../src'
 
 describe('reactive', () => {
 	it('happy path', () => {
@@ -56,5 +56,22 @@ describe('reactive', () => {
 		run()
 		// should have run
 		expect(dummy).toBe(2)
+	})
+
+	it('stop', () => {
+		let dummy
+		const obj = reactive({ prop: 1 })
+		const runner = effect(() => {
+			dummy = obj.prop
+		})
+		obj.prop = 2
+		expect(dummy).toBe(2)
+		stop(runner)
+		obj.prop = 3
+		expect(dummy).toBe(2)
+
+		// stopped effect should still be manually callable
+		runner()
+		expect(dummy).toBe(3)
 	})
 })
--- src/reactivity/src/effect.ts
+++ src/reactivity/src/effect.ts
@@ -1,8 +1,10 @@
-const targetsMap = new Map()
+const targetMap = new Map()
 let activeEffect
 
 class ReactiveEffect {
 	private _fn: any
+	public deps: any = []
+	private active: boolean = true
 	constructor(_fn, public scheduler?) {
 		this._fn = _fn
 	}
@@ -11,6 +13,13 @@ class ReactiveEffect {
 		activeEffect = this
 		return this._fn()
 	}
+
+	stop() {
+		if (this.active) {
+			this.active = false
+			cleanupEffect(this)
+		}
+	}
 }
 
 export function effect(fn, options: any = {}) {
@@ -18,33 +27,50 @@ export function effect(fn, options: any = {}) {
 
 	_effect.run()
 
-	return _effect.run.bind(_effect)
+	const runner = _effect.run.bind(_effect)
+	runner._effect = _effect
+
+	return runner
 }
 
 export function track(target, key) {
-	let targetMap = targetsMap.get(target)
-	if (!targetMap) {
-		targetMap = new Map()
-		targetsMap.set(target, targetMap)
-	}
-
-	let depsMap = targetMap.get(key)
+	let depsMap = targetMap.get(target)
 	if (!depsMap) {
-		depsMap = new Set()
-		targetMap.set(key, depsMap)
+		depsMap = new Map()
+		targetMap.set(target, depsMap)
 	}
 
-	depsMap.add(activeEffect)
+	let dep = depsMap.get(key)
+	if (!dep) {
+		dep = new Set()
+		depsMap.set(key, dep)
+	}
+
+	if (!activeEffect) return
+
+	dep.add(activeEffect)
+	activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key, value) {
-	let targetMap = targetsMap.get(target)
-	let depsMap = targetMap.get(key)
-	for (const effect of depsMap) {
+	let depsMap = targetMap.get(target)
+	let dep = depsMap.get(key)
+	for (const effect of dep) {
 		if (effect.scheduler) {
 			effect.scheduler()
 		} else {
 			effect.run()
 		}
 	}
+}
+
+export function stop(runner) {
+	runner._effect.stop()
+}
+
+function cleanupEffect(effect) {
+	effect.deps.forEach(dep => {
+		dep.delete(effect)
+	})
+	effect.deps.length = 0
 }
