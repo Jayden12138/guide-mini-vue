8bc9540cf0274a9e4fa7136f927aec9f8a95a450
Jayden12138 <18993882884@163.com>
2024-03-19 17:45:26 +0800

    ref

--- src/reactivity/src/index.ts
+++ src/reactivity/src/index.ts
@@ -7,3 +7,4 @@ export {
 	isProxy,
 } from './reactive'
 export { effect, stop } from './effect'
+export { ref } from './ref'
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,3 +3,5 @@ export const extend = Object.assign
 export function isObject(value) {
 	return value && typeof value === 'object'
 }
+
+export const hasChanged = Object.is
